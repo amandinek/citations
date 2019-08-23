296fc145be4c5ab8853358316a54d97ab89efc97
amandinek <karambiziga@gmail.com>
2019-08-23 14:27:46 +0200

    add new component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     showDescription:boolean;
-    constructor(public name:string,public description:string){
+    constructor(public name:string,public description:string, public copmleteDate:Date){
         this.showDescription=false;
     }
     
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,15 +8,23 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes('Change your thoughts and you change your world.',' Norman Vincent '),
-    new Quotes('Those who realize their folly are not true fools. ','Zhuangzi '),
-    new Quotes('It always seems impossible until it’s done. ','Nelson Mandela '),
-    new Quotes('It’s better to be a lion for a day than a sheep all your life.','Elizabeth Kenny')
+    new Quotes('Change your thoughts and you change your world.',' Norman Vincent ',new Date(2019,8,23)),
+    new Quotes('Those who realize their folly are not true fools. ','Zhuangzi ',new Date(2019,8,23)),
+    new Quotes('It always seems impossible until it’s done. ','Nelson Mandela ',new Date(2019,8,23)),
+    new Quotes('It’s better to be a lion for a day than a sheep all your life.','Elizabeth Kenny',new Date(2019,8,23))
 
   ];
   
   toggleDetails(index){
     this.quotes[index].showDescription= ! this.quotes[index].showDescription
+  }
+  deleteQuote(isComplete,index){
+    if(isComplete){
+      let toDelete =confirm(`Do you want to delete the quote`)
+      if(toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
   }
   
   constructor() { }
